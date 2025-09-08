c6885c7c9719c5f0a299d7d9aa5b550b8c2b764d
JelencsityMiklous <jelencsitymiklos@turr.hu>
2025-09-08 10:56:00 +0200

    push

---
+++ controllers/userCtrl.js
@@ -0,0 +1,23 @@
+function registration(){
+    alert('A')
+}
+
+function login(){
+    
+}
+
+function logout(){
+
+}
+
+function getProfile(){
+
+}
+
+function updateProfile(){
+
+}
+
+function updatePassword(){
+    
+}
